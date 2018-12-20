4c3ae57e6a4da1457591d781e531e1df4414a728
SacredBier <sacardbier@gmail.com>
2018-12-20 18:59:26 +0800

    supermarket

--- src/main/java/com/soft1841/sm/Main.java
+++ src/main/java/com/soft1841/sm/Main.java
@@ -1,9 +1,11 @@
 package com.soft1841.sm;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +13,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setTitle("盐系freshman");
+        primaryStage.setScene(new Scene(root, 1280, 853));
+        primaryStage.getIcons().add(new Image("/img/2.png"));
         primaryStage.show();
     }
 
