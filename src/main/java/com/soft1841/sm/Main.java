528cff17edabd3ef46e16966819f640164b07869
mdn81777 <45104583+mdn81777@users.noreply.github.com>
2018-12-21 01:08:21 +0800

    super=market

--- src/main/java/com/soft1841/sm/Main.java
+++ src/main/java/com/soft1841/sm/Main.java
@@ -1,6 +1,5 @@
 package com.soft1841.sm;
 
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,9 +8,7 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
         primaryStage.setTitle("盐系freshman");
         primaryStage.setScene(new Scene(root, 1280, 853));
@@ -23,4 +20,5 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-}
+    }
+
