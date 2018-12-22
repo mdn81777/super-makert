8b4d60d3816b2efe9d64b7df48ed1efc7cd06361
mdn81777 <45104583+mdn81777@users.noreply.github.com>
2018-12-23 01:12:13 +0800

    后台管理系统

--- src/main/java/com/soft1841/sm/dao/SupplierDAO.java
+++ src/main/java/com/soft1841/sm/dao/SupplierDAO.java
@@ -1,4 +1,41 @@
 package com.soft1841.sm.dao;
 
+import cn.hutool.db.Entity;
+import cn.hutool.db.SqlConnRunner;
+import com.soft1841.sm.entity.Supplier;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface SupplierDAO {
+    /**
+     * 查询所有供应商信息
+     * @return
+     * @throws SecurityException
+     */
+    List<Entity> selectSuppler() throws  SecurityException;
+
+    /**
+     * 新增供应商，返回自增主键
+     * @param supplier
+     * @return
+     * @throws SQLException
+     */
+    Long insetSupplier(Supplier supplier)throws SQLException;
+
+    /**
+     * 根据id删除供应商实体
+     * @param id
+     * @return
+     * @throws SQLException
+     */
+    int deleteSupplierById(long id) throws  SQLException;
+
+    /**
+     * 跟新供货商信息
+     * @param supplier
+     * @return
+     * @throws SQLException
+     */
+    int Supplier(Supplier supplier) throws SQLException;
 }
--- src/main/java/com/soft1841/sm/dao/TypeDAO.java
+++ src/main/java/com/soft1841/sm/dao/TypeDAO.java
@@ -35,7 +35,7 @@ public interface TypeDAO {
     Entity getTypeByID(int id) throws  SQLException;
 
     /**
-     * 根据id修改类别信息
+     * 更新类别信息
      * @param id
      * @return
      * @throws SQLException
