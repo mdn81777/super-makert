199d3610c6c475d0806093f9e05a7b4b9259dec3
SacredBier <sacardbier@gmail.com>
2018-12-23 01:16:12 +0800

    staffDAO

--- src/main/java/com/soft1841/sm/dao/StaffDAO.java
+++ src/main/java/com/soft1841/sm/dao/StaffDAO.java
@@ -1,4 +1,49 @@
 package com.soft1841.sm.dao;
 
+import cn.hutool.db.Entity;
+import com.soft1841.sm.entity.Staff;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface StaffDAO {
+    /**
+     * 写入新员工
+     * @param staff
+     * @return
+     * @throws SQLException
+     */
+    Integer insertStaff(Staff staff) throws SQLException;
+
+    /**
+     * 根据员工号删除员工
+     * @param staffId
+     * @return
+     * @throws SQLException
+     */
+    int deleteStaff(long staffId)throws SQLException;
+
+    /**
+     * 查询所有员工
+     * @return
+     * @throws SQLException
+     */
+    List<Entity> selectAllStaff()throws SQLException;
+
+    /**
+     * 通过ID查询某一员工
+     * @param staffId
+     * @return
+     * @throws SQLException
+     */
+    Entity getStaffById(long staffId)throws SQLException;
+
+    /**
+     * 根据员工号更新员工数据
+     * @param staffID
+     * @return
+     * @throws SQLException
+     */
+    int updateStaff(long staffID)throws SQLException;
+
 }
