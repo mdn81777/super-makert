ffcd531276fef8e185c208ed617297e5c58aceb5
mdn81777 <45104583+mdn81777@users.noreply.github.com>
2018-12-24 00:04:08 +0800

    mn change them last

---
+++ src/main/java/com/soft1841/sm/dao/impl/TypeDAOimpl.java
@@ -0,0 +1,38 @@
+package com.soft1841.sm.dao.impl;
+
+import cn.hutool.db.Db;
+import cn.hutool.db.Entity;
+import com.soft1841.sm.dao.TypeDAO;
+import com.soft1841.sm.entity.Type;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class TypeDAOimpl implements TypeDAO {
+    @Override
+    public Long insertType(Type type) throws SQLException {
+        return Db.use().insertForGeneratedKey(
+                Entity.create("t_type").set("type_name",type.getTypeName())
+        );
+    }
+
+    @Override
+    public int deleteTypeById(long id) throws SQLException {
+        return Db.use().del(Entity.create("t_type").set("id",id));
+    }
+
+    @Override
+    public List<Entity> selectAllTypes() throws SQLException {
+        return Db.use().query("SELECT * FROM t_type");
+    }
+
+    @Override
+    public Entity getTypeByID(int id) throws SQLException {
+        return Db.use().queryOne("SELECT * FROM t_type WHERE id = ?",id);
+    }
+
+    @Override
+    public int updateTypeById(int id) throws SQLException {
+        return 0;
+    }
+}
