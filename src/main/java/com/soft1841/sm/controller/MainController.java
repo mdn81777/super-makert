c01e32a1dc2b43637a314976481f55e2bd269b82
SacredBier <sacardbier@gmail.com>
2018-12-24 11:42:55 +0800

    yyt change them last

--- src/main/java/com/soft1841/sm/controller/MainController.java
+++ src/main/java/com/soft1841/sm/controller/MainController.java
@@ -1,7 +1,18 @@
 package com.soft1841.sm.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 /**
  * 主控制器 按钮切换页面等
  */
 public class MainController {
+    @FXML
+    private Button closeButton;
+
+    public void closeBtnOnAction(){
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
 }
