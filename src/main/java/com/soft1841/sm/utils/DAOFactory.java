1b85bd0017c8db92f7296eb6ce90c570baa94cbe
yechengxi <1401489328@qq.com>
2018-12-24 14:47:18 +0800

    DAO工厂

--- src/main/java/com/soft1841/sm/utils/DAOFactory.java
+++ src/main/java/com/soft1841/sm/utils/DAOFactory.java
@@ -1,4 +1,15 @@
 package com.soft1841.sm.utils;
 
+import com.soft1841.sm.dao.*;
+import com.soft1841.sm.dao.impl.*;
+
 public class DAOFactory {
+    public static DetailDAO getDetailDAOInstance(){ return new DetailDAOImpl(); }
+    public static GoodsDAO getGoodsDAOInstance(){ return new GoodsDAOImpl(); }
+    public static MemberDAO getMemberDAOInstance(){ return new MemberDAOImpl(); }
+    public static PositionDAO getPositionDAOInstance(){ return new PositionDAOImpl(); }
+    public static ReceiptDAO getReceiptDAOInstance(){ return new ReceiptDAOImpl(); }
+    public static StaffDAO getStaffDAOInstance(){ return new StaffDAOImpl(); }
+    public static SupplierDAO getSupplierDAOInstance(){ return new SupplierDAOImpl(); }
+    public static TypeDAO getTypeDAOInstance(){ return new TypeDAOImpl(); }
 }
