b06e09f1f6bf04f79eaa411ebe3b8af6fa1f4613
yechengxi <1401489328@qq.com>
2018-12-24 11:21:49 +0800

    some changes

---
+++ src/main/java/com/soft1841/sm/dao/impl/ReceiptDAOImpl.java
@@ -0,0 +1,33 @@
+package com.soft1841.sm.dao.impl;
+
+import cn.hutool.db.Db;
+import cn.hutool.db.Entity;
+import com.soft1841.sm.dao.ReceiptDAO;
+import com.soft1841.sm.entity.Receipt;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ReceiptDAOImpl implements ReceiptDAO {
+    @Override
+    public Long insertReceipt(Receipt receipt) throws SQLException {
+        return Db.use().insertForGeneratedKey(
+                Entity.create("t_receipt")
+                        .set("employee_id", receipt.getEmployeeId())
+                        .set("member_id", receipt.getMemberId())
+                        .set("time", receipt.getTime())
+                        .set("total", receipt.getTotal())
+        );
+
+    }
+
+    @Override
+    public Entity getReceiptById(Long id) throws SQLException {
+        return Db.use().queryOne("SELECT * FROM t_receipt WHERE id = ?");
+    }
+
+    @Override
+    public List<Entity> getAllReceipt() throws SQLException {
+        return Db.use().query("SELECT * FROM t_receipt");
+    }
+}
